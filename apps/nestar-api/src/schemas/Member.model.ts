b1ae5bf82455ff4d8244f3e382a64340bdab86c8
navacodez <navacodez@gmail.com>
2025-06-07 23:56:30 +0900

    feat: create Schema Models and project enums

---
+++ apps/nestar-api/src/schemas/Member.model.ts
@@ -0,0 +1,115 @@
+import { Schema } from "mongoose";
+import { MemberAuthType, MemberStatus, MemberType } from "../libs/enums/member.enum";
+
+const MemberSchema = new Schema({
+    memberType: {
+        type: String,
+        enum: MemberType,
+        defoult: MemberType.USER,
+    },
+
+    MemberStatus: {
+        type: String,
+        enum: MemberType,
+        defoult: MemberStatus.ACTIVE,
+    },
+
+    MemberAuthType: {
+        type: String,
+        enum: MemberType,
+        defoult: MemberAuthType.PHONE,
+    },
+
+    memberPhone: {
+        type: String,
+        index: { unique: true, sparse: true },
+        required: true,
+    },
+    
+    memberNick: {
+        type: String,
+        index: { unique: true, sparse: true },
+        required: true,
+    },
+
+    memberPassword: {
+        type: String,
+        select: false,
+        required: true,
+    },
+
+    memberFullName: {
+        type: String,
+    },
+
+    memberImage: {
+        type: String,
+        default: '',
+    },
+
+    memberAdress: {
+        type: String,
+    }
+    ,
+    memberDesc: {
+        type: String,
+    },
+
+    memberProperties: {
+        type: Number,
+        default: 0,
+    },
+
+    memberFollowers: {
+        type: Number,
+        default: 0,
+    },
+
+    memberFollowings: {
+        type: Number,
+        default: 0,
+    },
+
+    memberPoints: {
+        type: Number,
+        default: 0,
+    },
+
+    memberLikes: {
+        type: Number,
+        default: 0,
+    },
+
+    memberViews: {
+        type: Number,
+        default: 0,
+    },
+
+    memberComments: {
+        type: Number,
+        default: 0,
+    },
+
+    memberRanks: {
+        type: Number,
+        default: 0,
+    },
+
+    memberWarnings: {
+        type: Number,
+        default: 0,
+    },
+
+    memberBlocks: {
+        type: Number,
+        default: 0,
+    },
+
+    deleteAt: {
+        type: Date,
+    },
+},
+    { timestamps: true, collaction: 'members' },
+);
+
+export default MemberSchema;

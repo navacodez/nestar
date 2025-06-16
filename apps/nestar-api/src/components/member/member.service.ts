3cf37e3191970169819e930b6899c24bddd84c0a
navacodez <navacodez@gmail.com>
2025-06-16 12:59:03 +0900

    fix: integrate global pipe validation

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -28,7 +28,8 @@ export class MemberService {
         const response: Member = await this.memberModel
         .findOne({ memberNick: memberNick })
         .select('+memberPassword')
-        .exec() as unknown as Member;
+        .exec(); Member
+        
 
         if(!response || response.memberStatus === MemberStatus.DELETE) {
             throw new InternalServerErrorException(Message.NO_MEMBER_NICK);

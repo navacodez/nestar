9d1086e201f214fdad7188753e505859b1c86e04
navacodez <navacodez@gmail.com>
2025-07-02 20:16:50 +0900

    feat: develop likeTargteMember GraphQL api

--- apps/nestar-api/src/components/member/member.module.ts
+++ apps/nestar-api/src/components/member/member.module.ts
@@ -1,16 +1,15 @@
 import { Module } from '@nestjs/common';
 import { MemberResolver } from './member.resolver';
 import { MemberService } from './member.service';
-import { Mongoose } from 'mongoose';
-import { MemberStatus } from '../../libs/enums/member.enum';
 import { MongooseModule } from '@nestjs/mongoose';
 import MemberSchema from '../../schemas/Member.model';
 import { AuthModule } from '../auth/auth.module';
 import { ViewModule } from '../../libs/dto/view/view.module';
+import { LikeModule } from '../like/like.module';
 
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'Member', schema: MemberSchema }]), AuthModule, ViewModule],
+  imports: [MongooseModule.forFeature([{ name: 'Member', schema: MemberSchema }]), AuthModule, ViewModule, LikeModule],
   providers: [MemberResolver, MemberService],
   exports: [MemberService],
 })

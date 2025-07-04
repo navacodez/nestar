978ec0c9ab0c7ca0be9e16854778b3bf716dbe11
navacodez <navacodez@gmail.com>
2025-07-04 20:19:28 +0900

    feat: create WebSocket Connection process

---
+++ apps/nestar-api/src/socket/socket.gateway.ts
@@ -0,0 +1,28 @@
+import { Logger } from '@nestjs/common';
+import { OnGatewayInit, SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { Server } from 'http';
+
+@WebSocketGateway({ transports: ['websocket'], secure: false })
+export class SocketGateway implements OnGatewayInit {
+	private logger: Logger = new Logger('SocketEventsGateway');
+	private summaryClient: number = 0;
+
+	public afterInit(server: Server) {
+		this.logger.log(`Websocket Server Initialized total:${this.summaryClient}`);
+	}
+
+	handlerConnection(client: WebSocket, ...args: any[]) {
+		this.summaryClient++;
+		this.logger.log(`==Client connected total:${this.summaryClient}==`);
+	}
+
+	handlerDisconnect(client: WebSocket) {
+		this.summaryClient--;
+		this.logger.log(`Client disconnected total:${this.summaryClient}==`);
+	}
+
+	@SubscribeMessage('message')
+	public handlerMessage(client: WebSocket, payload: any): string {
+		return 'Hello World!';
+	}
+}
